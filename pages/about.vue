ab9a66d83d2f62b9c7f2d49e259103b709c3f6fa
Elliot McIntyre <35668113+lemcii@users.noreply.github.com>
2019-09-16 22:31:41 +0100

    feat(about): placeholder about page

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Little about me.
+    </div>
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            title: "About Elliot",
+        };
+    },
+};
+</script>
