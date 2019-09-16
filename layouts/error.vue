e666b5d578d76dc1e405401b81fb61b299ef16be
Elliot McIntyre <35668113+lemcii@users.noreply.github.com>
2019-09-16 22:31:30 +0100

    feat: basic error page

---
+++ layouts/error.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <span v-if="error.statusCode === 404">
+            Page not found.
+        </span>
+        <span v-else>
+            An error occurred.
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        error: {
+            type: Object,
+            default: null,
+        },
+    },
+    head() {
+        return {
+            title: this.message,
+        };
+    },
+    computed: {
+        statusCode() {
+            return (this.error && this.error.statusCode) || 500;
+        },
+        message() {
+            return this.error.message || `<%= messages.client_error %>`;
+        },
+    },
+};
+</script>
