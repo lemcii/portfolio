5250d1e5727a395cf660331880fde382db807b11
Elliot McIntyre <35668113+lemcii@users.noreply.github.com>
2019-09-17 18:54:58 +0100

    fix(credit): links were all zeit

--- components/Credit.vue
+++ components/Credit.vue
@@ -2,9 +2,9 @@
     <span class="font-light">
         <em class="italic">
             This site is hosted on
-            <a href="https://zeit.co" target="_blank">ZEIT</a>, made with
-            <a href="https://zeit.co" target="_blank">Tailwind CSS</a>,
-            <a href="https://zeit.co" target="_blank">Nuxt</a>
+            <a href="https://zeit.co" target="_blank" rel="noreferrer noopener">ZEIT</a>, made with
+            <a href="https://tailwindcss.com" target="_blank" rel="noreferrer noopener">Tailwind CSS</a>,
+            <a href="https://nuxtjs.org" target="_blank" rel="noreferrer noopener">Nuxt</a>
         </em>
     </span>
 </template>
