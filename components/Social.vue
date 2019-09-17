051c0dd1d8a26019f6ffa03c772609a0eae2d600
Elliot McIntyre <35668113+lemcii@users.noreply.github.com>
2019-09-17 18:57:03 +0100

    fix(social): add no-ref/no-opener rel values

--- components/Social.vue
+++ components/Social.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <a href="https://www.linkedin.com/in/ell10t" target="_blank" class="mr-4">LinkedIn</a>
-        <a href="https://github.com/ell10t" target="_blank" class="mr-4">GitHub</a>
-        <a href="https://twitter.com/ell10t_" target="_blank" class="mr-4">Twitter</a>
-        <a href="https://www.instagram.com/elliot.mcintyre" target="_blank">Instagram</a>
+        <a href="https://www.linkedin.com/in/ell10t" target="_blank" rel="noreferrer noopener" class="mr-4">LinkedIn</a>
+        <a href="https://github.com/ell10t" target="_blank" rel="noreferrer noopener" class="mr-4">GitHub</a>
+        <a href="https://twitter.com/ell10t_" target="_blank" rel="noreferrer noopener" class="mr-4">Twitter</a>
+        <a href="https://www.instagram.com/elliot.mcintyre" target="_blank" rel="noreferrer noopener">Instagram</a>
     </div>
 </template>
