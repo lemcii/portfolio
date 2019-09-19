aee92e99982457d9af0c47ff1c684f3741ba088a
Elliot McIntyre <35668113+lemcii@users.noreply.github.com>
2019-09-19 10:12:07 +0100

    refactor: apply classes to html/body tags instead of using css

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import "~/assets/css/main.css";
 import DefaultLayout from "~/layouts/Default.vue";
 
 export default function(Vue, { router, head, isClient }) {
+    head.htmlAttrs = { lang: "en", class: "h-full min-h-full bg-gray-900" };
+    head.bodyAttrs = { class: "h-full min-h-full bg-gray-900" };
+
     // Set default layout as a global component
     Vue.component("Layout", DefaultLayout);
 }
