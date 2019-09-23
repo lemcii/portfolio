3f296b2308fe271072d4ccbb184551fd33dad2ec
Elliot McIntyre <35668113+lemcii@users.noreply.github.com>
2019-09-23 16:43:59 +0100

    chore(social): updated links

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -1,8 +1,9 @@
 <template>
     <div>
-        <a href="https://www.linkedin.com/in/ell10t" target="_blank" rel="noreferrer noopener" class="mr-4">LinkedIn</a>
-        <a href="https://github.com/ell10t" target="_blank" rel="noreferrer noopener" class="mr-4">GitHub</a>
-        <a href="https://twitter.com/ell10t_" target="_blank" rel="noreferrer noopener" class="mr-4">Twitter</a>
+        <a href="https://www.linkedin.com/in/elliot-mcintyre" target="_blank" rel="noreferrer noopener" class="mr-4">LinkedIn</a>
+        <a href="https://github.com/lemcii" target="_blank" rel="noreferrer noopener" class="mr-4">GitHub</a>
+        <a href="https://twitter.com/lemcii" target="_blank" rel="noreferrer noopener" class="mr-4">Twitter</a>
+        <a href="https://www.twitch.tv/lemcii" target="_blank" rel="noreferrer noopener" class="mr-4">Twitch</a>
         <a href="https://www.instagram.com/elliot.mcintyre" target="_blank" rel="noreferrer noopener">Instagram</a>
     </div>
 </template>
